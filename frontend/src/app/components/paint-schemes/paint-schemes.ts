58502161a2a7e9546073fd36ec35983f77e29c4c
tim <timmiedema@icloud.com>
2026-04-23 05:46:04 +0200

    laaste form toegevoegd

--- frontend/src/app/components/paint-schemes/paint-schemes.ts
+++ frontend/src/app/components/paint-schemes/paint-schemes.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-paint-schemes',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './paint-schemes.html',
   styleUrl: './paint-schemes.css',
 })
-export class PaintSchemesComponent {}
+export class PaintSchemesComponent {
+  name = signal('');
+  description = signal('');
+
+  onSubmit() {
+    console.log('Nieuw schema:', {
+      name: this.name(),
+      description: this.description()
+    });
+    // Reset form
+    this.name.set('');
+    this.description.set('');
+  }
+}
