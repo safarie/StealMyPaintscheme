806902ad0ce48cd59c6e5d76d0696f5cf804755b
tim <timmiedema@icloud.com>
2026-05-27 19:36:38 +0200

    2 files waren niet toegevoegd

---
+++ backend/StealMyPaintscheme/StealMyPaintscheme.Api/Models/GlobalPaint.cs
@@ -0,0 +1,10 @@
+﻿namespace StealMyPaintscheme.Api.Models;
+
+public class GlobalPaint
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Type { get; set; }
+    public required string Maker { get; set; }
+    public required string RGB { get; set; }
+}
